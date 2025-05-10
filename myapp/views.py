bfd458404cba6d1812aa2f590bce60fc89ad84cf
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-05-10 20:41:27 +0700

    feat: added create

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import TodoItem
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .forms import MyForm
 
 
 # Create your views here.
-def home(request):
-    # return HttpResponse('hello world')
-    return render(request, "home.html")
-
-
 def todos(request):
     items = TodoItem.objects.all()
     return render(request, 'todos.html', {'todos': items})
+
+
+def create(request):
+    form = MyForm(request.POST)
+
+    if form.is_valid():
+        form.save()
+        return redirect(request, 'todos')
+    else:
+        form = MyForm()
+    return render(request, 'todos.html', {'form': form})
+
+
+
+
