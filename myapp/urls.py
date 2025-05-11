0c0d0b69c4fc84f4149361189120add8fc6a5deb
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-05-11 20:45:52 +0700

    add: url path

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('todos/', views.todos, name='todos'),
-    path('create_todo/', views.create_todo, name='create_todo')
+    path('create_todo/', views.create_todo, name='create_todo'),
+    path('edit/<id>', views.edit, name='edit')
 ]
