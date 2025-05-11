337b0ab1c4fdf87f2761cf7dd008b8b389d177ae
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-05-11 21:02:18 +0700

    add: 'delete' path

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('todos/', views.todos, name='todos'),
     path('create_todo/', views.create_todo, name='create_todo'),
-    path('edit/<id>', views.edit, name='edit')
+    path('edit/<id>', views.edit, name='edit'),
+    path('delete/<id>', views.delete, name='delete')
 ]
