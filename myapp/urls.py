86210f6ea7a4b19fa315c29340ae0cb9c7c66f92
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-05-11 10:29:01 +0700

    fix: url path for create todo

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todos/', views.todos, name='Todos'),
-    path('todos/create', views.create, name='Create')
+    path('todos/', views.todos, name='todos'),
+    path('create_todo/', views.create_todo, name='create_todo')
 ]
